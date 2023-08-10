d4bf8b8b9226f8900301d21e59a6b6d8e0d03a2c
tarek <th35@njit.edu>
2023-08-10 15:56:00 -0400

    updated portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Privacypolicy from '../styles/utils.module.css';
+import Signup from '../components/signupform';
+import PrivacyPolicy from '../pages/privacy';
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -14,34 +17,90 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Hi, my name is Tarek Hossain and]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+    <Head>
+      <title>{siteTitle}</title>
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-CE5Z19Q9BH"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CE5Z19Q9BH');
+            `
+          }}
+        />
+    </Head>
+   
+    <section className={`${utilStyles.headingMd} ${utilStyles.softBackground}`}>
+    <h2>Web Developer</h2>
+
+    <p>Hey there! I'm Tarek, a passionate front-end developer and creative thinker. With a knack for turning design concepts into interactive web experiences, I thrive on the intersection of design and code. </p>
+    
+    <h2>About Me</h2>
+      <p>I'm an enthusiastic and creative front-end developer, excited to contribute my skills to the world of web development. As a recent graduate with a strong foundation in HTML, CSS, and JavaScript, I'm passionate about crafting captivating user interfaces and bringing designs to life. When I'm not coding, you'll find me exploring the latest design trends, playing soccer, and dreaming up new digital innovations. My goal is to create seamless and user-centered web experiences that leave a lasting impact. Let's collaborate and make the web a more engaging place together!</p>
+
+    <h2>Professional Experience</h2>
+  <p>
+    As a Customer Support Specialist at Hallmark Aviation, I've honed my customer service skills to deliver exceptional support to our clients. I've become adept at understanding and addressing customer needs, resolving issues, and providing clear and effective solutions. I've consistently maintained a high customer satisfaction rate by demonstrating empathy, patience, and a genuine commitment to helping users overcome challenges. Through active listening and effective communication, I've been able to turn customer concerns into positive experiences, fostering long-lasting relationships.
+    </p>
+    <div className={utilStyles.projectSection}>
+  <h2>Projects</h2>
+  <div className={utilStyles.projectCards}>
+    <div className={`${utilStyles.projectCard} ${utilStyles.projectCardSameWidth}`}>
+          <h3>Restaurent Website</h3>
+      <p>This project marked a pivotal moment in my journey towards embracing web development as a career path. Constructed with a blend of HTML, CSS, JavaScript, and SQL, the website showcased my early exploration into these technologies. It boasted several interactive pages, including one where users could personally create their meals – an achievement that relied on SQL to preserve their culinary creations.</p>
+    </div>
+  </div>
+  <div className={utilStyles.projectCards}>
+    <div className={`${utilStyles.projectCard} ${utilStyles.projectCardSameWidth}`}>
+      <h3>Museum Website</h3>
+      <p>This was meticulously built using a combination of HTML and CSS, acting as a stepping stone towards my career in web development. This project beautifully exhibited my initial foray into these foundational technologies, reflecting my determination to learn and grow.</p>
+    </div>
+  </div>
+  <div className={utilStyles.projectCards}>
+    <div className={`${utilStyles.projectCard} ${utilStyles.projectCardSameWidth}`}>
+      <h3>Personal Website</h3>
+      <p>This project was built using bootstrap during an early stage of my web development career.  Its primary objective was to present a comprehensive portrait of my persona and professional prowess to potential recruiters. Fueled by the desire to encapsulate my essence and contributions, the website not only showcased my accomplishments but also provided an interactive platform for engagement. </p>
+    </div>
+  </div>
+</div>
+
+   <h2>SkillSet</h2>
+
+  <div>
+    <div>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>Python</li>
+      <li>SQL</li>
+      <li>Java</li>
+    </div>
+    <div>
+      <li>Git</li>
+      <li>GitHub</li>
+      <li>AutoCAD</li>
+      <li>Node.js</li>
+      <li>PHP</li>
+      <li>Amazon Web Services</li>
+    </div>
+  </div>
+
+    <Signup/>
+
+      <a href="https://www.linkedin.com/in/tarek-hossain-95b573254/">LinkedIn</a> <br></br>
+      <a href="https://github.com/Tarek514">GitHub</a>
+
+      <br></br>
+      <Link href="/privacy">
+          Privacy Policy
+      </Link>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
-      
-    </Layout>
+  </Layout>
   );
 }
---
+++ pages/privacy.js
@@ -0,0 +1,39 @@
+const PrivacyPolicy = () => {
+    return (
+        <div className="privacy-policy-container">
+            <h1>Privacy Policy for Tarek's Portfolio </h1>
+            <p>Greetings and welcome to my personal web developer portfolio. Safeguarding your privacy is of utmost significance to me. This privacy policy serves the purpose of acquainting you with the gathering, utilization, and safeguarding of personal data on this website, while adhering to GDPR guidelines.</p>
+
+            <h2>1. What Information is Collected</h2>
+            <p>As you explore this website, you have the option to maintain your anonymity without the need to disclose any personal information. Nevertheless, certain functionalities may require specific details from you. It's crucial to understand that we do not distribute your information to external parties such as advertising networks or platforms. Instances where sharing data may come into play encompass:</p>
+            <ul>
+                <li><strong>Contact form:</strong> If you opt to utilize the contact form, we may collect your email address along with any supplementary information you provide.</li>
+                <li><strong>Analytics:</strong> We utilize web analytics tools on our website to gain insights into user engagement, including the pages you visit and the time you spend on them. While we gather this information, user IP addresses are made anonymous in accordance with GDPR regulations.</li>
+            </ul>
+
+            <h2>2. How I Use Your Information</h2>
+            <p>Any personal information you provide is exclusively utilized for the specific purpose you have shared it. For instance:</p>
+            <ul>
+                <li>By reaching out to me through the contact form, I'll utilize the provided information to address and reply to your inquiry.</li>
+                <li>I analyze the data from analytics to gain insights into how visitors interact with the website and to implement enhancements.</li>
+            </ul>
+
+            <h2>3. Third-Party Links</h2>
+            <p>This website contains links to third-party websites such as Linkedin and Github. Clicking on these links or enabling such connections might lead to the collection or sharing of your data by third parties. Please note that I do not have control over these third-party websites and I cannot be held responsible for their privacy policies.</p>
+
+            <h2>4. Your Rights</h2>
+            <p>You have the right to:</p>
+            <ul>
+                <li>Request permission to access any personal information that I might have in my possession concerning you.</li>
+                <li>Request correction of any inaccurate data.</li>
+                <li>Object to your data being processed</li>
+            </ul>
+
+            <h2>5. Changes to This Privacy Policy</h2>
+            <p>I may make revisions to this privacy policy periodically. Any modifications will be shared on this page, so please consider checking it occasionally.</p>
+
+        </div>
+    );
+}
+
+export default PrivacyPolicy;
